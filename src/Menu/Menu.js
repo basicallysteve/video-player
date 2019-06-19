ce3cb12e64264495155be178784e355c0e5ffdb6
Steven Turner <stevenpaulturner@yahoo.com>
2019-06-19 08:19:44 -0400

    Create Documentation

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import './Menu.css'
+/**
+ * @class Menu
+ * @classdesc Stateless class that controls which video is being played
+ */
 export class Menu extends React.Component {
-  handleVideoChange (name) {
-    this.props.onChange(name)
+  /**
+    * @function handleVideoChange
+    * @param {number} id
+    * @description changes the main video that is being played onClick
+    */
+  handleVideoChange (id) {
+    this.props.onChange(id)
   }
-
+  /**
+  * @function preview
+  * @param {object} event
+  * @description Plays video on hover
+  */
   preview (event) {
     let video = event.target
     video.play()
   }
-
+  /**
+  * @function pause
+  * @param {object} event
+  * @description Pauses video onMouseLeave. Puts video back to 0
+  */
   pause (event) {
     let video = event.target
     video.currentTime = 0
     video.pause()
   }
+  /**
+  * @function render
+  * @description Renders component
+  */
   render () {
     return (
       <div className='thumbnails menu-container'>
--- src/Video/Video.js
+++ src/Video/Video.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './Video.css'
 export class Video extends React.Component {
+  /**
+  * @function handleLike
+  * @description Triggers like on video being played
+  */
   handleLike () {
     this.props.onLike(this.props.video.id)
   }
+  /**
+  * @function handleUnLike
+  * @description Triggers unlike on video being played
+  */
   handleUnlike () {
     console.log(this.props)
     this.props.onUnLike(this.props.video.id)
   }
+  /**
+  * @function render
+  * @description Renders component
+  */
   render () {
     let likeButton = this.props.video.isLiked === true ? <i onClick={this.handleUnlike.bind(this)} className='fas fa-heart like-button' /> : <i onClick={this.handleLike.bind(this)} className='far fa-heart like-button' />
     return (
